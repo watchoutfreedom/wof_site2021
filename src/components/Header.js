66c20e70b214d76641f67544ad6cbe41676d038e
wof <angel@watchoutfreedom.com>
2022-01-12 13:12:30 +0100

    bitcoin philosophy

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ export default function Header() {
             <Link href="/1">
                     <h1>WOF!</h1>
             </Link>
-                    <h3>The collective idea.</h3>
             <div className="links"><Link href="/0_projects">Projects</Link>/<Link href="/0_clients">Clients</Link>/<Link href="/">Blog</Link></div>
         </header>
     );
